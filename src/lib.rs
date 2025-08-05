978c2129a7462bbd3a38428da82db4d991cc64ad
dengpju <dengpju@163.com>
2025-08-05 15:28:52 +0800

    更新

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,7 @@ use std::sync::Mutex;
 use walkdir::WalkDir;
 
 lazy_static! {
-    static ref CONFIG_COLLECT: Mutex<BTreeMap<String, BTreeMap<String, serde_yml::Value>>> =
-        Mutex::new(BTreeMap::new());
+    static ref CONFIG_COLLECT: Mutex<BTreeMap<String, BTreeMap<String, serde_yml::Value>>> = Mutex::new(BTreeMap::new());
 }
 
 #[cfg(test)]
@@ -21,8 +20,8 @@ mod tests {
         print(|key, value| {
             println!("{:?}: {:?}", key, value);
         });
-        let version = get::<String>("app.SSL.OUT".to_string());
-        println!("{:?}", version);
+        let version = get::<String>("env.config.app.SSL.OUT.hhff".to_string());
+        println!("{} {:?}", line!(), version);
     }
 }
 
@@ -33,22 +32,33 @@ pub fn load_config(config_dir: String) {
         let file_path = entry.path();
         let extension = file_path.extension().and_then(|s| s.to_str());
 
+        let file_name = file_path.file_name().and_then(|s| s.to_str()).unwrap();
         // 检查扩展名是否为".yaml"
         if file_path.is_file() && extension == Some("yaml") {
             println!("{:?}", file_path);
+            let mut keys: Vec<String> = vec![];
+            for p in file_path.iter() {
+                if p.ne(".") {
+                    if p.ne(file_name) {
+                        keys.push(format!("{:?}", p).replace("\"", ""));
+                    } else {
+                        keys.push(format!("{:?}", p).replace(".yaml", "").replace("\"", ""));
+                    }
+                }
+            }
+
             let file_name = file_path.file_name().and_then(|s| s.to_str()).unwrap();
             let file_name = file_name.replace(".yaml", "");
             let yaml_content =
                 fs::read_to_string(file_path).expect(format!("读取{:?}失败", file_path).as_str());
 
             // 解析YAML内容到BTreeMap中，自动保持顺序
-            let deserialized_map: BTreeMap<String, serde_yml::Value> =
-                serde_yml::from_str(&yaml_content).unwrap();
+            let deserialized_map: BTreeMap<String, serde_yml::Value> = serde_yml::from_str(&yaml_content).unwrap();
 
             CONFIG_COLLECT
                 .lock()
                 .unwrap()
-                .insert(String::from(file_name), deserialized_map.clone());
+                .insert(String::from(keys.join(".")), deserialized_map.clone());
         }
     }
 }
@@ -64,77 +74,74 @@ pub fn print(f: PrintFn) {
 
 #[allow(unused)]
 pub fn get<'a, T: Deserialize<'a>>(key: String) -> Option<T> {
+    let mut result = None;
     if key.contains(".") {
         let keys = key.split(".").collect::<Vec<&str>>();
-        let mut config_collect: Mutex<BTreeMap<String, serde_yml::Value>> =
-            Mutex::new(BTreeMap::new());
+        let mut config_collect: Option<BTreeMap<String, serde_yml::Value>> = None;
         let mut value: Option<serde_yml::Value> = None;
         let mut mapping: Option<serde_yml::Mapping> = None;
-        let mut i = 0;
+
+        let mut temp_keys: Vec<String> = vec![];
+
         for key in keys {
-            println!("{:?}: {:?}", &key, i);
-            if i == 0 {
-                config_collect = Mutex::new(
-                    CONFIG_COLLECT
-                        .lock()
-                        .unwrap()
-                        .get(&key.to_string())
-                        .cloned()
-                        .unwrap(),
-                );
-            } else if let Some(m) = &mapping {
-                value = m.get(&key.to_string()).cloned();
-                println!("{} value {:?}", line!(), &value);
-                if value.is_some() {
-                    if value.clone().unwrap().is_mapping() {
-                        mapping = value.clone().unwrap().as_mapping().cloned();
-                    } else {
-                        mapping = None;
-                        return value.and_then(|v| T::deserialize(v).ok());
-                    }
-                } else {
-                    mapping = None;
-                    return value.and_then(|v| T::deserialize(v).ok());
-                }
-            } else if let Some(vv) = value.clone() {
-                if vv.is_mapping() {
-                    match vv.as_mapping() {
-                        Some(m) => {
-                            mapping = Some(m.clone());
-                            value = None;
-                        }
-                        None => return None,
-                    }
-                } else {
-                    return value.and_then(|v| T::deserialize(v).ok());
+            println!("{} {:?}", line!(), &key);
+            result = None;
+
+            temp_keys.push(key.to_string());
+
+            if config_collect.is_none() {
+                if let Some(bm) = CONFIG_COLLECT.lock().unwrap().get(&temp_keys.join(".").to_string()){
+                    config_collect = Some(bm.clone());
                 }
             } else {
-                value = config_collect
-                    .lock()
-                    .unwrap()
-                    .get(&key.to_string())
-                    .cloned();
-                println!("{} value {:?}", line!(), &value);
-                if let Some(vv) = value.clone() {
+                if let Some(m) = &mapping {
+                    value = m.get(&key.to_string()).cloned();
+                    println!("{} value {:?}", line!(), &value);
+                    if value.is_some() {
+                        if value.clone().unwrap().is_mapping() {
+                            mapping = value.clone().unwrap().as_mapping().cloned();
+                            value = None;
+                        } else {
+                            result = value.clone().and_then(|v| T::deserialize(v).ok());
+                        }
+                    } else {
+                        result = value.clone().and_then(|v| T::deserialize(v).ok());
+                        value = None;
+                    }
+                } else if let Some(vv) = value.clone() {
                     if vv.is_mapping() {
                         match vv.as_mapping() {
                             Some(m) => {
                                 mapping = Some(m.clone());
                                 value = None;
                             }
-                            None => return None,
+                            None => (),
                         }
                     } else {
-                        return value.and_then(|v| T::deserialize(v).ok());
+                        result = value.clone().and_then(|v| T::deserialize(v).ok());
+                        value = None;
                     }
                 } else {
-                    return None;
+                    value = config_collect.clone().unwrap().get(&key.to_string()).cloned();
+                    println!("{} value {:?}", line!(), &value);
+                    if let Some(vv) = value.clone() {
+                        if vv.is_mapping() {
+                            match vv.as_mapping() {
+                                Some(m) => {
+                                    mapping = Some(m.clone());
+                                    value = None;
+                                }
+                                None => (),
+                            }
+                        } else {
+                            println!("{} value {:?}", line!(), &value);
+                            result = value.clone().and_then(|v| T::deserialize(v).ok());
+                            value = None;
+                        }
+                    }
                 }
             }
-            i += 1;
         }
-        None
-    } else {
-        None
     }
+    result
 }
